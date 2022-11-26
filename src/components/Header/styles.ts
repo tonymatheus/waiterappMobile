0e16338a9253e910e1da8950fa5d4f8b2fc8c130
tonymatheus <tony.chaves14@gmail.com>
2022-11-26 01:06:11 -0300

    Implement new  styles of Header's component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,3 +3,19 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   margin: 24px 24px 0;
 `;
+
+export const OrderContent = styled.View``;
+
+export const OrderHeader = styled.View`
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const Table = styled.View`
+  background: #fff;
+  padding: 16px;
+  border: 1px solid rgba(204, 204, 204, 0.3);
+  border-radius: 8px;
+  margin-top: 24px;
+`;
