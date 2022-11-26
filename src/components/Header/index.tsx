9eda0baf3666b0a3f0592f315c56c9db507eefc9
tonymatheus <tony.chaves14@gmail.com>
2022-11-26 01:05:35 -0300

    Pass props and conditional rendering to component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { Text } from '../Text';
-import { Container } from './styles';
+import { Container, OrderContent, OrderHeader, Table } from './styles';
 
-export const Header = () => {
+interface HeaderProps {
+  selectedTable: string;
+  onCancelOrder: () => void;
+}
+
+export const Header = ({ selectedTable, onCancelOrder }: HeaderProps) => {
   return (
     <Container>
-      <Text size={14} opacity={0.9}>
-        Bem Vindo(a) Ao
-      </Text>
-      <Text size={24} weight="700">
-        Waiter
-        <Text>App</Text>
-      </Text>
+      {!selectedTable && (
+        <>
+          <Text size={14} opacity={0.9}>
+            Bem Vindo(a) Ao
+          </Text>
+          <Text size={24} weight="700">
+            Waiter
+            <Text>App</Text>
+          </Text>
+        </>
+      )}
+      {selectedTable && (
+        <OrderContent>
+          <OrderHeader>
+            <Text size={24} weight="600">
+              Pedido
+            </Text>
+            <TouchableOpacity onPress={onCancelOrder}>
+              <Text size={14} weight="600" color="#d73035">
+                Cancelar Pedido
+              </Text>
+            </TouchableOpacity>
+          </OrderHeader>
+          <Table>
+            <Text size={16} color="#666" weight="400">
+              Mesa {selectedTable}
+            </Text>
+          </Table>
+        </OrderContent>
+      )}
     </Container>
   );
 };
