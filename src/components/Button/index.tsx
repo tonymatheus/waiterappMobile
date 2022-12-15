9fa9760908fb0276602d39880bfe962f42ced987
tonymatheus <tony.chaves14@gmail.com>
2022-12-15 06:01:55 -0300

    Pass loading condition to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Container } from './styles';
 import { Text } from '../Text';
+import { ActivityIndicator } from 'react-native';
 
 interface Props {
   children: string;
   onPress: () => void;
   disabled?: boolean;
+  loading?: boolean;
 }
 
-export const Button = ({ children, onPress, disabled, ...rest }: Props) => {
+export const Button = ({
+  children,
+  onPress,
+  disabled,
+  loading,
+  ...rest
+}: Props) => {
   return (
-    <Container {...rest} onPress={onPress} disabled={disabled}>
-      <Text weight="600" color="#fff">
-        {children}
-      </Text>
+    <Container {...rest} onPress={onPress} disabled={disabled || loading}>
+      {!loading && (
+        <Text weight="600" color="#fff">
+          {children}
+        </Text>
+      )}
+      {loading && <ActivityIndicator size={20} color="#fff" />}
     </Container>
   );
 };
